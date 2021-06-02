9234afb98311e5824c13bde252c63664d6a97c32
manishpal022 <manishpal.055@gmail.com>
2021-06-02 12:46:35 +0530

    first commit

---
+++ backend/urls.py
@@ -0,0 +1,19 @@
+
+from backend.settings import MEDIA_ROOT
+import base
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import Settings, settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('api/', include('base.urls')),
+    path('api/products/', include('base.urls.product_urls')),
+    path('api/users/', include('base.urls.user_urls')),
+    path('api/orders/', include('base.urls.order_urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ base/products.py
@@ -0,0 +1,18 @@
+products = [
+    {
+      "id": "1",
+      "name": "Tom Cruise",
+      "age": 56,
+      "Born At": "Syracuse, NY",
+      "Birthdate": "July 3, 1962",
+      "photo": "https://jsonformatter.org/img/tom-cruise.jpg"
+    },
+    {
+      "id": "2",
+      "name": "Robert Downey Jr.",
+      "age": 53,
+      "Born At": "New York City, NY",
+      "Birthdate": "April 4, 1965",
+      "photo": "https://jsonformatter.org/img/Robert-Downey-Jr.jpg"
+    }
+]
